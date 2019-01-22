5a93e97e054037a47a0af065c2113f367840b632
RobertK66 <Robert.climb@rkos.at>
2019-01-22 22:35:36 +0100

    Initial Hardwaretest Project Structure (Single mbed board only)

---
+++ MyMbedHardwaretest/src/board/board_impl.c
@@ -0,0 +1,17 @@
+/*
+ * board_impl.c
+ *
+ *  Created on: 22.01.2019
+ *      Author: Robert
+ */
+
+#include "my_board_api.h"
+
+void MyBoard_SysInit(void) {
+
+}
+
+void MyBoard_Init(void) {
+
+}
+
---
+++ my_board_api/inc/my_board_api.h
@@ -0,0 +1,14 @@
+/*
+ * my_board_api.h
+ *
+ *  Created on: 22.01.2019
+ *      Author: Robert
+ */
+
+#ifndef MY_BOARD_API_H_
+#define MY_BOARD_API_H_
+
+void MyBoard_SysInit(void);
+void MyBoard_Init(void);
+
+#endif /* MY_BOARD_API_H_ */
