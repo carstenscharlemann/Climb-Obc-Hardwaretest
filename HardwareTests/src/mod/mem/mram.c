e38c8eb3fa4f7e492c73f7f3cb141725b9958999
RobertK66 <robert.climb@rkos.at>
2019-12-27 16:44:32 +0100

    new module

---
+++ HardwareTests/src/mod/mem/mram.c
@@ -0,0 +1,8 @@
+/*
+ * mram.c
+ *
+ *  Created on: 27.12.2019
+ *      Author: Robert
+ */
+
+
---
+++ HardwareTests/src/mod/mem/mram.h
@@ -0,0 +1,13 @@
+/*
+ * mram.h
+ *
+ *  Created on: 27.12.2019
+ *      Author: Robert
+ */
+
+#ifndef MOD_MEM_MRAM_H_
+#define MOD_MEM_MRAM_H_
+
+
+
+#endif /* MOD_MEM_MRAM_H_ */
